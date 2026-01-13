a2ba1626893e0aa1f1292d3b81d64eb992f47e22
JulieVera2026 <julieverabt21@gmail.com>
2026-01-13 11:39:25 -0800

    Initial Commit

---
+++ WordGarden App/WordGarden_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_AppApp.swift
+//  WordGarden App
+//
+//  Created by VERA, JULIE on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
